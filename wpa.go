0cdc8de26d0d7feff899da1487ec5ff4e3f8261f
Marco don Kaulea <DonMarco42@gmail.com>
2013-11-19 23:52:55 +0100

    Inital Code Commit

---
+++ wpa.go
@@ -0,0 +1,26 @@
+/*  Package containing functions to calculate keys used in WPA1/2 PSK
+    Copyright (C) 2013 Marco 'don' Kaulea
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+package wpa
+
+import "code.google.com/p/go.crypto/pbkdf2"
+import "crypto/sha1"
+
+func DeriveWPAPMK(passphrase, salt string) []byte {
+	return pbkdf2.Key([]byte(passphrase), []byte(salt), 4096, 32, sha1.New)
+}
